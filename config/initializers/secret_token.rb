9ca1bc85399e4346196fc411f9b3ae9d1d9ca968
apalzab <apalzab@gmail.com>
2014-02-22 18:18:55 +0100

    Changes for heroku production

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SqueezeIt::Application.config.secret_key_base = 'cbfdfdbec7afbc9830ed9f648b0f0390f620ad8e95f9092993274fcb23d7551dfbda09a8d54ec1972a96bfff04346ef68feb8cc0820be728be46e7979dbcbe31'
