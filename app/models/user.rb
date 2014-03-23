d501d68166a2b136235414461c8711ab8062ae23
apalzab <apalzab@gmail.com>
2014-03-23 20:17:26 +0100

    Several updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :squeezes#, dependent: :destroy
- 
+
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
-                                   class_name:  "Relationship",
-                                   dependent:   :destroy
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+
+  has_many :followed_users, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships, source: :follower
 
   def following?(other_user)
