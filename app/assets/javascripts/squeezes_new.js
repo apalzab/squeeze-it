51e3d228eb5df1096247c78a040a56634a13ccc7
apalzab <apalzab@gmail.com>
2014-10-26 21:52:07 +0100

    new squeeze improved

--- app/assets/javascripts/squeezes_new.js
+++ app/assets/javascripts/squeezes_new.js
@@ -1,6 +1,7 @@
 $(".squeezes.new, .squeezes.edit").ready(function() {
    $('.lights span').click(function() {
       if ( $(this).attr('class') == 'off' ) {
+        $(this).text('Turn on the lights');
         $(this).removeClass('off');
         $(this).addClass('on');
         $('body').css('background', 'rgba(0,0,0,.8)');
@@ -9,6 +10,7 @@ $(".squeezes.new, .squeezes.edit").ready(function() {
       } else {
         $(this).removeClass('on');
         $(this).addClass('off');
+        $(this).text('Turn off the lights');
         $('body').css('background', 'white');
         $('#squeeze_title').removeClass('dark');
         $('.actions .save').removeClass('dark');
